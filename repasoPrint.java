9091c08e6b016d2364518f6f50948cf32f0646e1
FABIAN L. FLOREZ G. <fabian.florez03@uptc.edu.co>
2026-04-30 13:54:46 -0500

    Add repasoPrint.java with print statements

---
+++ repasoPrint.java
@@ -0,0 +1,47 @@
+//---------------------------------------//
+//--FABIAN LEONARDO FLOREZ GONZALEZ------//
+//---CODIGO 202614060--------------------//
+//---------------------------------:D----//
+package repasoPrint;
+
+public class repasoPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. Imprimir Hola mundo
+		System.out.println("Hola mundo\n"); 
+		//2. Imprimir Nombre y apellido
+		System.out.println("Fabian Leonardo Florez Gonzalez\n"); 
+		//3. Nombre, edad, ciudad en su propio println
+		System.out.println("Fabian"); 
+		System.out.println("19"); 
+		System.out.println("Soatá\n"); 
+		//4. Frase de 5 palabras
+		System.out.println("La tierra no nos pertenece\n"); 		
+		//5. Imprimier A, B y C
+		System.out.println("======== PUNTO 5 ========");
+		System.out.println("A"); 
+		System.out.println("B"); 
+		System.out.println("C\n"); 
+		System.out.println("======== PUNTO 6 ========");
+		//5. Imprimir resultado 5+3
+		System.out.println("El resultado de 5+3 es "+ (5+3)+"\n");
+		//6. Java es genial 3 veces
+		System.out.println("1. java es genial");
+		System.out.println("2. java es genial");
+		System.out.println("3. java es genial");
+		//7. Imprimir tabla en java
+        System.out.println("+---------------+---------------+---------------+");
+        System.out.println("|\t1\t|\tF\t|\t5\t|");
+        System.out.println("+---------------+---------------+---------------+");
+        System.out.println("|\t2\t|\tH\t|\t0\t|");
+        System.out.println("+---------------+---------------+---------------+");
+        System.out.println("|\t3\t|\tJ\t|\t2\t|");
+        System.out.println("+---------------+---------------+---------------+");
+
+		
+
+
+	}
+
+}
